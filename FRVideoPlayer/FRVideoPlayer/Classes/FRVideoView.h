f29638aa5f367290d2008bbb37a33333fa81b6ab
coldmn3 <coldmn3@gmail.com>
2017-08-21 00:03:42 +0800

    toggle

--- FRVideoPlayer/FRVideoPlayer/Classes/FRVideoView.h
+++ FRVideoPlayer/FRVideoPlayer/Classes/FRVideoView.h
@@ -10,8 +10,24 @@
 
 #import "FRVideoControlViewProtocol.h"
 
+typedef NS_ENUM(NSInteger, FRVideoViewState) {
+    FRVideoViewStateWindowed,
+    FRVideoViewStateFullscreen,
+    FRVideoViewStateToggling
+};
+
 @interface FRVideoView : UIView
 
 @property (nonatomic, strong) UIView<FRVideoControlView> *controlView;
+
+@property (nonatomic, assign) FRVideoViewState viewState;
+
+@property (nonatomic, weak) UIView *viewParentView;
+
+@property (nonatomic, assign) CGRect windowedFrame;
+
+@property (nonatomic, assign) CGFloat toggleAnimationDuration;
+
+- (void)toggleView;
 
 @end
--- FRVideoPlayer/FRVideoPlayer/ViewController.m
+++ FRVideoPlayer/FRVideoPlayer/ViewController.m
@@ -20,6 +20,15 @@
     
     self.videoView = [[FRVideoView alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 240)];
     [self.view addSubview:self.videoView];
+    
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
+    [self.videoView addGestureRecognizer:tapGestureRecognizer];
+}
+
+- (void)handleTapGesture:(UITapGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        [self.videoView toggleView];
+    }
 }
 
 - (BOOL)shouldAutorotate {
